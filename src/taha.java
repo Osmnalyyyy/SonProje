5a4048d905282040ca0287ce6bdff9099d134cc9
Osmnalyyyy <108487432+Osmnalyyyy@users.noreply.github.com>
2022-08-21 22:03:49 +0300

    vson

--- src/taha.java
+++
@@ -1,7 +0,0 @@
-public class taha {
-    public static void main(String[] args) {
-        System.out.println("Hocam oldu insallah");
-          System.out.println("Hocam oldu insallah");
-          System.out.println("Hocam oldu insallah");
-    }
-}
