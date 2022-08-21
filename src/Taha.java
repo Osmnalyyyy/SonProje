e3fd438f9139e7df3e22c486b8efafcdd676ff97
Taha Turkmenoglu <tturkmenoglu1@gmail.com>
2022-08-21 23:12:02 +0400

    2022/08/21

--- src/Taha.java
+++ src/Taha.java
@@ -1,3 +1,11 @@
 public class Taha {
+    public static void main(String[] args) {
+
+        System.out.println("Taha Classi");
+        1();
+    }
+    public static void 1(){
+        System.out.println("Birinici method calisti");
+    }
 
 }
