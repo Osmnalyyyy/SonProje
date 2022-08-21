ab0f50e72a3cb54a78e8ae4af09c2b9e33ee48a6
Ahmet Guzel <gzlhmt@gmail.com>
2022-08-21 22:17:55 +0300

    dsdffs

--- src/Ahmet.java
+++ src/Ahmet.java
@@ -1,6 +1,6 @@
 public class Ahmet {
     public static void main(String[] args) {
-
+        ahmet();
     }
     public static void ahmet() {
         System.out.println("artık git hub kullanabiliyoruz \n   süpeeerrr");
